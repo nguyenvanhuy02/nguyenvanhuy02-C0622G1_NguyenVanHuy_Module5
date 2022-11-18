98b262f9612a226fd423dac0aee3330ddabc19d6
nguyenvanhuy02 <maohuy123312@gmail.com>
2022-11-18 15:21:03 +0700

    nbt

---
+++ src/ss2_typescript/exercise/SumFibonacci.js
@@ -0,0 +1,22 @@
+function sumFibonacci() {
+    var numberOne = 0;
+    var numberTwo = 1;
+    var arr = [0, 1];
+    var check = 2;
+    while (true) {
+        var numberThree = numberOne + numberTwo;
+        arr.push(numberThree);
+        numberOne = numberTwo;
+        numberTwo = numberThree;
+        check++;
+        if (check > 9) {
+            break;
+        }
+    }
+    var sum = 0;
+    arr.forEach(function (value) {
+        sum += value;
+    });
+    console.log(sum);
+}
+sumFibonacci();
---
+++ src/ss2_typescript/exercise/SumFibonacci.ts
@@ -0,0 +1,22 @@
+function sumFibonacci() {
+    let numberOne:number=0;
+    let numberTwo:number=1;
+    let arr:number[]=[0,1];
+    let check:number=2;
+    while (true){
+        let numberThree:number=numberOne+numberTwo;
+        arr.push(numberThree);
+        numberOne=numberTwo;
+        numberTwo=numberThree;
+        check++;
+        if (check>9){
+            break;
+        }
+    }
+    let sum:number=0;
+    arr.forEach(function (value) {
+        sum+=value;
+    })
+    console.log(sum);
+}
+sumFibonacci();
